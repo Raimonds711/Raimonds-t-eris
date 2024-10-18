c05f8911f8d91697c236d7e2791dabfcde491de2
Raimonds <147736950+Raimonds711@users.noreply.github.com>
2024-10-18 12:39:35 +0300

    Add files via upload

---
+++ uzdevums2.py
@@ -0,0 +1,9 @@
+pareiza_parole = ("python123")
+while True:
+    parole = input("Ievadiet savu paroli.")
+
+    if parole == pareiza_parole:
+        print("Piekļuve ir atļauta ")
+        break
+    else:
+         print("Piekļuve nav atļauta")
